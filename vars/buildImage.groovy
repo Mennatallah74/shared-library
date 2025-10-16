9ec3d846b846fee061146a95465e34b57baf37ae
Mennatallah74 <mennaali7400@gmail.com>
2025-10-16 09:11:56 +0200

    adding params

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,9 +1,9 @@
 #!/user/bin/env groovy
-def call() {
+def call(string imageName) {
     echo 'building the docker image...'
     withCredentials([usernamePassword(credentialsId: 'docker-secrets', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t menna213/demo-app:3.0 .'
+        sh "docker build -t $imageName ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push menna213/demo-app:3.0'
+        sh "docker push $imageName"
     }
 }
